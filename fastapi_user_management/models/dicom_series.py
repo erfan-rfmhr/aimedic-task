71d3cc549e68339bc7a2cea81a9e257730ec6f2a
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-06-09 12:13:50 +0330

    feat(dicom_series table): create dicom_series model and revision

---
+++ fastapi_user_management/models/dicom_series.py
@@ -0,0 +1,22 @@
+"""dicom_series table."""
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+
+from fastapi_user_management.models.base import Base
+from fastapi_user_management.models.user_role import UserRoleModel  # noqa: F401
+
+
+class DicomSeriesModel(Base):
+    """DicomSeriesModel known as dicom_series table."""
+
+    __tablename__ = "dicom_series"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    StudyInstanceUID: Mapped[str] = mapped_column(String)
+    SeriesInstanceUID: Mapped[str] = mapped_column(String)
+    Modality: Mapped[str] = mapped_column(String)
+    BodyPartExamined: Mapped[str] = mapped_column(String)
+    PatientID: Mapped[int] = mapped_column(Integer, ForeignKey("user_accounts.id"))
